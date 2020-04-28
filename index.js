a339d0531919d60cae3855fc9d64b5546df040ae
Adrien Degliame <adidf-web@laposte.net>
2020-04-28 15:43:00 +0200

    Integrate launcher.json new fields

--- index.js
+++ index.js
@@ -5,8 +5,13 @@ const path = require("path");
 function createWindow () {
   // Create main window
   let win = new BrowserWindow({
+    title: launcherConfig.WINDOW_SETTING.WINDOW_NAME,
     width: launcherConfig.WINDOW_SETTING.WINDOW_SIZE.WIDTH,
     height: launcherConfig.WINDOW_SETTING.WINDOW_SIZE.HEIGHT,
+    resizable: launcherConfig.WINDOW_SETTING.RESIZABLE,
+    fullscreen: launcherConfig.WINDOW_SETTING.FULLSCREEN,
+    frame: !launcherConfig.WINDOW_SETTING.FRAMELESS,
+    icon: path.resolve(__dirname, "public", launcherConfig.WINDOW_SETTING.WINDOW_ICON),
     webPreferences: {
       nodeIntegration: launcherConfig.ELECTRON_SETTINGS.NODE_INTEGRATION
     }
